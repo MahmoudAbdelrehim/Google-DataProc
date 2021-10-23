0a438ab44d62014cd70f65bc4141bc4a4bbd3169
GBMA95 <audy1995@gmail.com>
2021-10-23 23:48:15 +0200

    Creating storage and Dataproc cluster

---
+++ data_proc.tf
@@ -0,0 +1,71 @@
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+resource "google_storage_bucket" "spark-nv" {
+  project       = var.project_id
+  name          = "rapids-sparknv"
+  location      = var.region
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+resource "google_dataproc_cluster" "accelerated_cluster" {
+  project  = var.project_id
+  provider = google-beta
+  name     = "rapidsai-spark-nvgpu"
+  region   = var.region
+  cluster_config {
+    endpoint_config {
+      enable_http_port_access = "true"
+    }
+    gce_cluster_config {
+      zone = "${var.region}-b"
+      metadata = {
+        rapids-runtime      = "SPARK"
+        gpu-driver-provider = "NVIDIA"
+      }
+    }
+    staging_bucket = google_storage_bucket.spark-nv.name
+    master_config {
+      num_instances = 1
+      machine_type  = "n1-standard-16"
+    }
+    worker_config {
+      accelerators {
+        accelerator_type  = "nvidia-tesla-t4"
+        accelerator_count = "2"
+      }
+      num_instances = 2
+      machine_type  = "n1-standard-8"
+      disk_config {
+        num_local_ssds = 1
+      }
+    }
+    initialization_action {
+      script = "gs://goog-dataproc-initialization-actions-${var.region}/gpu/install_gpu_driver.sh"
+    }
+    initialization_action {
+      script = "gs://goog-dataproc-initialization-actions-${var.region}/rapids/rapids.sh"
+    }
+    initialization_action {
+      script = "gs://goog-dataproc-initialization-actions-${var.region}/kafka/kafka.sh"
+    }
+    software_config {
+      image_version       = "2.0-ubuntu18"
+      optional_components = [ "JUPYTER", "ZEPPELIN", "DOCKER", "PRESTO", "FLINK"]
+    }
+
+  }
+
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "prefab-rampart-301014"
+region     = "europe-west4"
